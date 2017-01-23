002e479bbdd937e4550471ac6b4fe489105fa827
Onur Yener <onurch@hotmail.com>
2017-01-23 07:08:42 +0100

    Ich habe den Help Dialog ohne Datenbankanbindung gemacht.

---
+++ src/ch/iso/m426/controller/HelpAboutEventHandler.java
@@ -0,0 +1,29 @@
+package ch.iso.m426.controller;
+
+import ch.iso.m426.model.Deck;
+
+import java.util.Optional;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Dialog;
+
+
+
+public class HelpAboutEventHandler implements EventHandler<ActionEvent>{
+
+	@Override
+	public void handle(ActionEvent arg0) {
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("Information Dialog");
+		alert.setHeaderText("Card Manager");
+		alert.setContentText("Das ist ein Card Manager. Dieser wurde von 4 genialen Köpfen gemacht:David Bart, David Wyss, Serafima und Ugur.");
+
+		alert.showAndWait();
+		
+	}
+
+
+
+}
--- src/ch/iso/m426/view/CardManagerMenu.java
+++ src/ch/iso/m426/view/CardManagerMenu.java
@@ -1,6 +1,8 @@
 package ch.iso.m426.view;
 
 import ch.iso.m426.controller.DeckManagementEventHandler;
+import ch.iso.m426.controller.HelpAboutEventHandler;
+import ch.iso.m426.controller.HelpEventHandler;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
@@ -42,7 +44,9 @@ public class CardManagerMenu extends MenuBar {
 		Menu help = new Menu(Constants.MENU_TITLE_HELP);
 
 		MenuItem helpHelp = new MenuItem(Constants.SUB_MENU_TITLE_HELP_HELP);
+		helpHelp.setOnAction(new HelpEventHandler());
 		MenuItem helpAbout = new MenuItem(Constants.SUB_MENU_TITLE_HELP_ABOUT);
+		helpAbout.setOnAction(new HelpAboutEventHandler());
 		
 		// Add Items to MenuEntrys
 		help.getItems().addAll(helpHelp, helpAbout);
