d44e3ad70cc6598f60896ebbdb1e2097b5146f25
David .B <darobba@hotmail.com>
2017-01-23 09:36:46 +0100

    Added Exiteventhandler

--- src/ch/iso/m426/controller/DeckManagementEventHandler.java
+++ src/ch/iso/m426/controller/DeckManagementEventHandler.java
@@ -1,15 +1,20 @@
 package ch.iso.m426.controller;
 
-import ch.iso.m426.model.Deck;
 import ch.iso.m426.view.DeckTableView;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.text.Text;
 
-import java.util.Arrays;
+public class DeckManagementEventHandler implements EventHandler<ActionEvent> {
 
-public class DeckManagementEventHandler {
+    private final BorderPane cardManagerBorderPane;
 
-    public static void manageDeck(BorderPane CardManagerBorderPane) {
-        CardManagerBorderPane.setCenter(new DeckTableView());
+    public DeckManagementEventHandler(BorderPane cardManagerBorderPane) {
+        this.cardManagerBorderPane = cardManagerBorderPane;
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+        this.cardManagerBorderPane.setCenter(new DeckTableView());
     }
 }
