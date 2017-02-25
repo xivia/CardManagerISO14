e463742d04d3785e835135d9f908bfde0fb48614
David .B <darobba@hotmail.com>
2017-02-25 23:55:21 +0100

    Decks are now removeable -> Added DeleteButton in DeckTableView

---
+++ src/ch/iso/m426/view/DeleteButton.java
@@ -0,0 +1,27 @@
+package ch.iso.m426.view;
+
+import ch.iso.m426.model.DatabaseHandler;
+import ch.iso.m426.model.DeckObservableList;
+import javafx.scene.control.Button;
+
+public class DeleteButton extends Button {
+
+    public DeleteButton(){
+        super();
+
+        this.setText("Löschen");
+        this.setOnAction(event -> {
+            try {
+                String name = DeckObservableList.getTable().getSelectionModel().getSelectedItem().getName();
+                DatabaseHandler.deleteDeck(name);
+                DeckObservableList.get().clear();
+                DatabaseHandler.getAllDecks();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+
+        });
+
+    }
+
+}
